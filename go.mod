f628510b0c9d1c0bad59c91f4e9a7148621f1493
Timofei Kayukov <ktima1804@gmail.com>
2026-07-18 19:15:29 +0300

    init go module

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/thwqsz/agent-gateway
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/thwqsz/agent-gateway/handlers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /ask", handlers.AskHandler)
+
+	log.Println("Go-шлюз запущен на :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
